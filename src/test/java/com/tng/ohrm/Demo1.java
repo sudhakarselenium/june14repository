ac716261008eb1729030c13dd081a4fa55d3283b
sudhakarselenium <me@qtpsudhakar.com>
2017-07-28 08:45:32 +0530

    updated demo

--- src/test/java/com/tng/ohrm/Demo1.java
+++ src/test/java/com/tng/ohrm/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this demo");
+		System.out.println("new modification");
 	}
 
 }
